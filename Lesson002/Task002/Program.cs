da43ede169f2f15051cc9a4374fe170580233925
Any4rd <Any4rd@gmail.com>
2023-06-16 18:14:01 +0300

    [+] ДЗ №2

---
+++ Lesson002/Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+string GetThirdDigit(int number)
+{
+    string ConvertNumber = Convert.ToString(number);
+    switch (ConvertNumber.Length)
+    {
+        case > 2:
+            return $"Вторя цифра: {ConvertNumber[2]};";
+
+        default:
+            return "Третьей цифры нет";
+    }
+}
+
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetThirdDigit(number));
---
+++ Lesson002/Task003/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+string DayOfTheWeek(int day)
+{
+    switch (day)
+    {
+        case > 0 and < 6:
+            return "Будний день недели";
+
+        case 6:
+        case 7:
+            return "Выходной";
+
+        default:
+            return "Это не день недели";
+    }
+}
+
+Console.Write("Введи номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DayOfTheWeek(day));
