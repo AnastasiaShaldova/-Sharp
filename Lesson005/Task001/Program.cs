212d5ea624f7527679414020e18dbc8e9443b4bf
Any4rd <Any4rd@gmail.com>
2023-06-30 14:08:43 +0300

    [+] ДЗ

---
+++ Lesson005/Task001/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 
+*/ 
+
+void Print(int[] numbers)
+{
+    Console.Write("[");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int InputNumber(){
+    Console.WriteLine("Введите число: "); 
+    int input = Convert.ToInt32(Console.ReadLine()); 
+    return input; 
+}
+
+void RandArray(int numb)
+{   
+    int[] RandomArray = new int[numb];
+    for (int i = 0; i < numb; i++)
+    {
+        RandomArray[i] = new Random().Next(99, 1000);
+    }
+    ShowNumber(RandomArray); 
+    Print(RandomArray); 
+}
+
+void ShowNumber(int[] numbers){
+    int count = 0; 
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            count += 1; 
+        }
+    }
+    Console.WriteLine($"Количество четных чисел: {count}"); 
+}
+
+RandArray(InputNumber()); 
+
+
---
+++ Lesson005/Task002/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 
+*/
+
+void Print(int[] numbers)
+{
+    Console.Write("[");
+
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]}, ");
+    }
+
+    Console.Write("]");
+}
+
+int InputNumber()
+{
+    Console.WriteLine("Введите число: "); 
+    int input = Convert.ToInt32(Console.ReadLine()); 
+    return input; 
+}
+
+void RandArray(int numb)
+{   
+    int[] RandomArray = new int[numb];
+
+    for (int i = 0; i < numb; i++)
+    {
+        RandomArray[i] = new Random().Next(99, 1000);
+    }
+
+    ShowNumber(RandomArray); 
+    Print(RandomArray);
+}
+
+void ShowNumber(int[] numbers){
+    int sum = 0; 
+    for (int i = 1; i < numbers.Length; i+=2)
+    {
+        sum = sum + numbers[i];
+    }
+
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}"); 
+}
+
+RandArray(InputNumber()); 
+
