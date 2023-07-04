d399e873140714ae1a9d3ca23b9c2f1470ca34af
Any4rd <Any4rd@gmail.com>
2023-07-04 09:56:37 +0300

    [+] ДЗ №7

---
+++ Lesson007/Task002/Program.cs
@@ -0,0 +1,56 @@
+﻿/* 
+Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет 
+*/
+
+Console.Clear(); 
+
+int [,] CreateArray(int[,] array){ 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            array[i,j] = new Random().Next(-10, 10); 
+        } 
+    } 
+    return array; 
+} 
+
+void ElementSearch(int[,] array, int line, int column){ 
+    if (line > array.GetLength(0) || column > array.GetLength(0)) {
+        Console.WriteLine("Такого числа в массиве нет"); 
+    }
+    else {
+        Console.WriteLine($"Значение элемента в массиве: {array[line - 1, column - 1]}");
+    }
+
+} 
+
+void PrintArray(int [,] array){ 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            Console.Write($"{array [i,j]}\t"); 
+        } 
+        Console.WriteLine(); 
+    } 
+} 
+Console.WriteLine("Введите № строки: ");
+int line = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите № столбца: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[5, 5];
+
+CreateArray(numbers);
+PrintArray(CreateArray(numbers));
+ElementSearch(numbers, line, column); 
---
+++ Lesson007/Task003/Program.cs
@@ -0,0 +1,59 @@
+﻿/* 
+Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+Console.Clear(); 
+
+int [,] CreateArray(int[,] array){ 
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            array[i,j] = new Random().Next(1, 10); 
+        } 
+    } 
+    return array; 
+} 
+
+void ArithmeticMeanColumn(int[,] array, int column){ 
+    
+    for (int i = 0; i < array.GetLength(0); i++) 
+    { 
+        double SumColumn = 0; 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        { 
+            SumColumn = (SumColumn + array[i, j]); 
+        } 
+        SumColumn = SumColumn / column; 
+        Console.Write($"{SumColumn}; "); 
+    } 
+    Console.WriteLine(); 
+} 
+
+void PrintArray(int [,] array){ 
+    for (int i = 0; i < array.GetLength(1); i++) 
+    { 
+        for (int j = 0; j < array.GetLength(0); j++) 
+        { 
+            Console.Write($"{array [j,i]}\t"); 
+        } 
+        Console.WriteLine(); 
+    } 
+} 
+Console.WriteLine("Введите количество строк: ");
+int line = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[line, column];
+
+CreateArray(numbers);
+PrintArray(CreateArray(numbers));
+ArithmeticMeanColumn(numbers, column); 
