1c60a6bc3043bbe61588114824feb1866f2c465c
Any4rd <Any4rd@gmail.com>
2023-07-10 11:56:45 +0300

    [+] ДЗ №9

---
+++ Lesson009/Task001/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+Console.Clear(); 
+
+string ReturnRealDigits(int startNum, int endNum){ 
+    if(startNum <= endNum) return startNum.ToString(); 
+    return startNum + " " + ReturnRealDigits(startNum-1, endNum); // " " для преобразования всего рядом в string 
+} 
+
+Console.WriteLine("Введите начало: ");
+int startNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конец: ");
+int endNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ReturnRealDigits(startNum, endNum));
---
+++ Lesson009/Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Clear(); 
+
+int ReturnSumDigits(int startNum, int endNum){ 
+    if(startNum <= endNum) 
+        return startNum; 
+    else 
+        return startNum + ReturnSumDigits(startNum+1, endNum); 
+}
+
+Console.WriteLine("Введите начало: ");
+int startNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конец: ");
+int endNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ReturnSumDigits(startNum, endNum));
